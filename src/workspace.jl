1681b636b57830d08977e69af0f2f354d3e5d654
bkincaid256 <bekincai@ncsu.edu>
2020-11-02 15:07:34 -0500

    Added more convenience functions and a subpar SWAP function.

--- src/workspace.jl
+++ src/workspace.jl
@@ -21,13 +21,6 @@ function makeregister(nqubits::Int)
     return [Qubit(i, copy(zerovec)) for i ∈ 1:nqubits]
 end
 ######Single qubit gates ###########
-function Hadamard!(ψ::Qubit)
-    #Hadamard gate for a single qubit    
-    H = @SMatrix [1/sqrt(2)  1/sqrt(2)
-                  1/sqrt(2) -1/sqrt(2)] 
-    ψ.vec .= H*ψ.vec
-    return ψ
-end
 
 function H!(C::Circuit, bitpos::Int)
     #=Implementation that adds the new matrix into the gates field 
@@ -53,100 +46,43 @@ function H!(C::Circuit, bitpos::Int)
    return push!(C.gates, M)
 end
 
-function H!(C::Circuit, set::Vector{Int})
-    #=Implementation that adds the new matrix into the gates field 
-    of the circuit=#
+function H!(C::Circuit, set::AbstractVector{Int})
+    #=Implementation that gives user ability to define 
+    a range of qubits to affect with the specified gate=#
     for i in set
         H!(C,i)
     end
 end
 
-function H!(C::Circuit, set::UnitRange)
+function Ugate!(C::Circuit, U::AbstractArray{ComplexF64,2}, bitpos::Int)
     #=Implementation that adds the new matrix into the gates field 
     of the circuit=#
+    a = length(C.qreg)
+    U = sparse(U)
+    M = similar(U)
+    I2 = sparse(I*(1.0+0.0*im),2,2)
+    for  i ∈ 1:a
+        if (i == 1) && (i !== bitpos)
+            copy!(M,I2)
+        elseif (i == 1) && (i == bitpos)
+            copy!(M,U)
+        else  
+            if (i == bitpos)
+                M = kron(U,M)
+            else
+                M = kron(I2,M)
+            end
+        end
+    end
+   return push!(C.gates, M)
+end
+
+function Ugate!(C::Circuit, U::AbstractArray{ComplexF64,2}, set::AbstractVector{Int})
+    #=Implementation that gives user ability to define 
+    a range of qubits to affect with the specified gate=#
     for i in set
-        H!(C,i)
+        Ugate!(C,U,i)
     end
-end
-######## Pauli Gates ##########
-function Xgate!(ψ::Qubit)
-    #X Pauli gate for a single qubit
-    X =  @SMatrix [0.0 1.0
-                   1.0 0.0]
-    ψ.vec .= X*ψ.vec
-    return ψ
-end
-
-function X!(C::Circuit, bitpos::Int)
-    #=Implementation that adds the new matrix into the gates field 
-    of the circuit=#
-    a = length(C.qreg)
-    X = sparse([0.0+0.0*im  1.0+0.0*im
-                1.0+0.0*im  0.0+0.0*im]) 
-    M = similar(X)
-    I2 = sparse(I*(1.0+0.0*im),2,2)
-    for  i ∈ 1:a
-        if (i == 1) && (i !== bitpos)
-            copy!(M,I2)
-        elseif (i == 1) && (i == bitpos)
-            copy!(M,X)
-        else  
-            if (i == bitpos)
-                M = kron(X,M)
-            else
-                M = kron(I2,M)
-            end
-        end
-    end
-   return push!(C.gates, M)
-end
-
-function Y!(C::Circuit, bitpos::Int)
-    #=Implementation that adds the new matrix into the gates field 
-    of the circuit=#
-    a = length(C.qreg)
-    Y = sparse([0.0+0.0*im  0.0-1.0*im
-                0.0+1.0*im  0.0+0.0*im]) 
-    M = similar(Y)
-    I2 = sparse(I*(1.0+0.0*im),2,2)
-    for  i ∈ 1:a
-        if (i == 1) && (i !== bitpos)
-            copy!(M,I2)
-        elseif (i == 1) && (i == bitpos)
-            copy!(M,Y)
-        else  
-            if (i == bitpos)
-                M = kron(Y,M)
-            else
-                M = kron(I2,M)
-            end
-        end
-    end
-   return push!(C.gates, M)
-end
-
-function Z!(C::Circuit, bitpos::Int)
-    #=Implementation that adds the new matrix into the gates field 
-    of the circuit=#
-    a = length(C.qreg)
-    Z = sparse([1.0+0.0*im  0.0+0.0*im
-                0.0+0.0*im -1.0+0.0*im]) 
-    M = similar(Z)
-    I2 = sparse(I*(1.0+0.0*im),2,2)
-    for  i ∈ 1:a
-        if (i == 1) && (i !== bitpos)
-            copy!(M,I2)
-        elseif (i == 1) && (i == bitpos)
-            copy!(M,Z)
-        else  
-            if (i == bitpos)
-                M = kron(Z,M)
-            else
-                M = kron(I2,M)
-            end
-        end
-    end
-   return push!(C.gates, M)
 end
 
 function T!(C::Circuit, bitpos::Int)
@@ -173,21 +109,31 @@ function T!(C::Circuit, bitpos::Int)
    return push!(C.gates, M)
 end
 
-function Ugate!(C::Circuit, U::AbstractArray{ComplexF64,2}, bitpos::Int)
+function T!(C::Circuit, set::AbstractVector{Int})
+    #=Implementation that gives user ability to define 
+    a range of qubits to affect with the specified gate=#
+    for i in set
+        T!(C,i)
+    end
+end
+
+######## Pauli Gates ##########
+function X!(C::Circuit, bitpos::Int)
     #=Implementation that adds the new matrix into the gates field 
     of the circuit=#
     a = length(C.qreg)
-    U = sparse(U)
-    M = similar(U)
+    X = sparse([0.0+0.0*im  1.0+0.0*im
+                1.0+0.0*im  0.0+0.0*im]) 
+    M = similar(X)
     I2 = sparse(I*(1.0+0.0*im),2,2)
     for  i ∈ 1:a
         if (i == 1) && (i !== bitpos)
             copy!(M,I2)
         elseif (i == 1) && (i == bitpos)
-            copy!(M,U)
+            copy!(M,X)
         else  
             if (i == bitpos)
-                M = kron(U,M)
+                M = kron(X,M)
             else
                 M = kron(I2,M)
             end
@@ -196,9 +142,81 @@ function Ugate!(C::Circuit, U::AbstractArray{ComplexF64,2}, bitpos::Int)
    return push!(C.gates, M)
 end
 
-############# Controlled Gates #############
-function CX!(C::Circuit,control::Int,target::Int)
+function X!(C::Circuit, set::AbstractVector{Int})
+    #=Implementation that gives user ability to define 
+    a range of qubits to affect with the specified gate=#
+    for i in set
+        X!(C,i)
+    end
+end
 
+function Y!(C::Circuit, bitpos::Int)
+    #=Implementation that adds the new matrix into the gates field 
+    of the circuit=#
+    a = length(C.qreg)
+    Y = sparse([0.0+0.0*im  0.0-1.0*im
+                0.0+1.0*im  0.0+0.0*im]) 
+    M = similar(Y)
+    I2 = sparse(I*(1.0+0.0*im),2,2)
+    for  i ∈ 1:a
+        if (i == 1) && (i !== bitpos)
+            copy!(M,I2)
+        elseif (i == 1) && (i == bitpos)
+            copy!(M,Y)
+        else  
+            if (i == bitpos)
+                M = kron(Y,M)
+            else
+                M = kron(I2,M)
+            end
+        end
+    end
+   return push!(C.gates, M)
+end
+
+function Y!(C::Circuit, set::AbstractVector{Int})
+    #=Implementation that gives user ability to define 
+    a range of qubits to affect with the specified gate=#
+    for i in set
+        Y!(C,i)
+    end
+end
+
+function Z!(C::Circuit, bitpos::Int)
+    #=Implementation that adds the new matrix into the gates field 
+    of the circuit=#
+    a = length(C.qreg)
+    Z = sparse([1.0+0.0*im  0.0+0.0*im
+                0.0+0.0*im -1.0+0.0*im]) 
+    M = similar(Z)
+    I2 = sparse(I*(1.0+0.0*im),2,2)
+    for  i ∈ 1:a
+        if (i == 1) && (i !== bitpos)
+            copy!(M,I2)
+        elseif (i == 1) && (i == bitpos)
+            copy!(M,Z)
+        else  
+            if (i == bitpos)
+                M = kron(Z,M)
+            else
+                M = kron(I2,M)
+            end
+        end
+    end
+   return push!(C.gates, M)
+end
+
+function Z!(C::Circuit, set::AbstractVector{Int})
+    #=Implementation that gives user ability to define 
+    a range of qubits to affect with the specified gate=#
+    for i in set
+        Z!(C,i)
+    end
+end
+
+############# Controlled Gates and 2 qubit Gates#############
+function CX!(C::Circuit,control::Int,target::Int)
+    #Implentation of CNOT gate using Kronnecker products.
     X = sparse([0.0+0.0*im 1.0+0.0*im;
                 1.0+0.0*im 0.0+0.0*im])
 
@@ -258,8 +276,76 @@ function CX!(C::Circuit,control::Int,target::Int)
 
 end
 
-function Toffoli!(C::Circuit,c1::Int,c2::Int,target::Int)
+function Swap!(C::Circuit,b1::Int,b2::Int)
+    #quick and dirty implmentation of Swap Gate
+    CX!(C,b2,b1)
+    CX!(C,b1,b2)
+    CX!(C,b2,b1)
+end
 
+function CZ!(C::Circuit,control::Int,target::Int)
+    #Implentation of CZ gate using Kronnecker products.
+    Z = sparse([1.0+0.0*im  0.0+0.0*im;
+                0.0+0.0*im -1.0+0.0*im])
+
+    a = length(C.qreg)
+
+    if (target==control)
+        error("Target qubit and control qubit must be Different!")
+    elseif (target>a) || (control>a)
+        error("The target and control qubit must be within the size of the register!")
+    end
+
+    M1 = spzeros(ComplexF64,2,2)
+    M2 = copy(M1)
+    I2 = sparse(I*(1.0+0.0*im),2,2)
+
+    conket1 = sparse([0.0+0.0*im 0.0+0.0*im;
+                      0.0+0.0*im 1.0+0.0*im])
+    conket0 = sparse([1.0+0.0*im 0.0+0.0*im
+                      0.0+0.0*im 0.0+0.0*im])
+
+    for i ∈ 1:a
+
+        if (i==1)
+            if (i==control)
+                M1 = conket1
+            elseif (i==target)
+                M1 = Z
+            else
+                M1 = I2
+            end
+        else
+            if (i==control)
+                M1 = kron(conket1,M1)
+            elseif (i==target)
+                M1 = kron(Z,M1)
+            else
+                M1 = kron(I2,M1)
+            end
+        end
+    end
+
+    for i ∈ 1:a
+
+        if (i==1==control)
+            M2 = conket0
+        elseif (i==1!==control)
+            M2 = I2
+        else
+            if (i==control)
+                M2 = kron(conket0,M2)
+            else
+                M2 = kron(I2,M2)
+            end
+        end
+    end
+    return push!(C.gates,M1+M2)
+
+end
+
+function CCX!(C::Circuit,c1::Int,c2::Int,target::Int)
+    #Implementation of the Toffoli gate using Kronnecker products. 
     X = sparse([0.0+0.0*im 1.0+0.0*im;
                 1.0+0.0*im 0.0+0.0*im])
 
@@ -342,7 +428,7 @@ function Toffoli!(C::Circuit,c1::Int,c2::Int,target::Int)
             end
         end
     end
-    
+
     for i ∈ 1:a
 
         if (i==1)
